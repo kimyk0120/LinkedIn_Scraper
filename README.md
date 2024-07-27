29669c29852415635c967a8a551a667b8236febb
kimyk0120 <kimyk0120@hotmail.com>
2024-07-27 17:14:07 +0900

    * mod

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # LinkedIn_Scraper
+
+
+
+
+---
+### DONE
+- 프로젝트 init 
+- django 프로젝트 생성 및 기초 설정 
+
+### TODO
+- 인덱스 페이지 디자인
+
+
--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 
+def hello(request):
+    test_data = {"test": "this is str from server"}
+    return render(request, 'hello.html', test_data)
+
+
 def index(request):
     test_data = {"test": "this is str from server"}
-
     return render(request, 'index.html', test_data)
