b59f64c01a45b3352bbf4d597bcbdd29c8df7726
kimyk0120 <kimyk0120@hotmail.com>
2024-07-27 16:21:30 +0900

    * init app

--- Toyp_LinkedIn_Scraper/settings.py
+++ Toyp_LinkedIn_Scraper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cuv@45q3iusvi%jz748p3$a8l7724$$+gv#e1-#uiuh2%2-vbk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scraper',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
