62596987c106f4531029cb4937a64f2292f3af1e
kimyk0120 <kimyk0120@hotmail.com>
2024-08-23 13:24:19 +0900

    * mod

--- scraper/core.py
+++ scraper/core.py
@@ -1,20 +1,23 @@
-import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import pandas as pd
 from selenium.webdriver.common.by import By
-
-# selenium으로 키를 조작하기 위한 import
-from selenium.webdriver.common.keys import Keys
 
 
 def chrome(headless=False):
     opt = webdriver.ChromeOptions()
     if headless:
         opt.add_argument("--headless")
-    opt.add_experimental_option('excludeSwitches', ['enable-logging'])
-    opt.add_argument("--disable-popup-blocking")
+
+    opt.add_argument('--no-sandbox')
+    opt.add_argument("--disable-extensions")
+    opt.add_argument("disable-blink-features=AutomationControlled")  # 자동화 탐지 방지
+    opt.add_experimental_option("excludeSwitches", ["enable-automation"])  # 자동화 표시 제거
+    opt.add_experimental_option('useAutomationExtension', False)  # 자동화 확장 기능 사용 안 함
+    opt.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
+
     browser = webdriver.Chrome(options=opt)
+    browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     browser.implicitly_wait(10)
     return browser
 
@@ -23,9 +26,11 @@ def scraper():
     print("Hello World")
 
     test_url = "https://www.linkedin.com/in/suwaidaslam/"
+    # test_url = "https://www.linkedin.com/in/youngkwang-kim-360739244"
+
     print("Test URL: {}".format(test_url))
 
-    browser = chrome(False)
+    browser = chrome(headless=False)
 
     # 로그인
     # browser.set_window_position(2048, 0)  # 우측 세컨 모니터를 이용하기 위해 왼쪽 메인 모니터 width 만큼 이동
@@ -57,10 +62,14 @@ def scraper():
         current_scroll_position, new_height = 0, 1
         while current_scroll_position <= new_height:
             current_scroll_position += speed
-            browser.execute_script("window.scrollTo(0, {});".format(current_scroll_position))
-            new_height = browser.execute_script("return document.body.scrollHeight")
+            try:
+                browser.execute_script("window.scrollTo(0, {});".format(current_scroll_position))
+                new_height = browser.execute_script("return document.body.scrollHeight")
+            except Exception as e:
+                print("error scrolling down: {}".format(e))
+                break
 
-    scroll_down_page(speed=8)
+    scroll_down_page(8)
 
     src = browser.page_source
     soup = BeautifulSoup(src, 'lxml')
@@ -84,7 +93,10 @@ def scraper():
     # Get Location of the Person
     try:
         location_section = soup.findAll('section', {'class': 'artdeco-card'})[0]
-        location = location_section.find_all('div', recursive=False)[1].find_all('div', recursive=False)[1].find_all('div',recursive=False)[1].find('span').get_text().strip()
+        location = \
+            location_section.find_all('div', recursive=False)[1].find_all('div', recursive=False)[1].find_all('div',
+                                                                                                              recursive=False)[
+                1].find('span').get_text().strip()
         print("Location: {}".format(location))
     except Exception as e:
         print("error getting location: {}".format(e))
@@ -101,51 +113,92 @@ def scraper():
         print("error getting about: {}".format(e))
         about = None
 
-    # # Get Company Link of the Person
-    # try:
-    #     exp_section = soup.find('section', {'id': 'experience-section'})
-    #     exp_section = exp_section.find('ul')
-    #     li_tags = exp_section.find('div')
-    #     a_tags = li_tags.find('a')
-    #
-    #     company_link = a_tags['href']
-    #     company_link = 'https://www.linkedin.com/' + company_link
-    # except:
-    #     company_link = None
-    #
-    # # Get Job Title of the Person
-    # try:
-    #     job_title = li_tags.find('h3', {'class': 't-16 t-black t-bold'}).get_text().strip()
-    # except:
-    #     job_title = None
-    #
-    # # Get Company Name of the Person
-    # try:
-    #     company_name = li_tags.find('p',
-    #                                 {'class': 'pv-entity__secondary-title t-14 t-black t-normal'}).get_text().strip()
-    # except:
-    #     company_name = None
-    #
-    # contact_page = test_url + 'detail/contact-info/'
-    # browser.get(contact_page)
-    # browser.implicitly_wait(1)
-    #
-    # contact_card = browser.page_source
-    # contact_page = BeautifulSoup(contact_card, 'lxml')
-    # # Get Linkdin Profile Link and Contact details of the Person
-    # try:
-    #     contact_details = contact_page.find('section', {
-    #         'class': 'pv-profile-section pv-contact-info artdeco-container-card ember-view'})
-    #     contacts = []
-    #     for a in contact_details.find_all('a', href=True):
-    #         contacts.append(a['href'])
-    # except:
-    #     contacts.append('')
+    # Get Experience of the Person
+    try:
+        experience_tag = soup.select_one("#experience")
+        experience_lis = experience_tag.parent.select('div:nth-child(3) > ul > li')
+
+        experience_list = []
+        for (i, li) in enumerate(experience_lis):
+
+            try:
+                job_title_tag = li.select_one(
+                    'div > div:nth-child(2) > div:nth-child(1) > div > div > div span:first-child')
+                if job_title_tag is None:
+                    job_title = li.select_one(
+                        'div > div:nth-child(2) > div:nth-child(1) > a > div > div > div span:first-child').get_text().strip()
+                else:
+                    job_title = job_title_tag.get_text().strip()
+            except Exception as e:
+                job_title = None
+
+            try:
+                company_name_tag = li.select_one(
+                    'div > div:nth-child(2) > div:nth-child(1) > div > span:nth-child(2) > span')
+                if company_name_tag is None:
+                    company_name = li.select_one(
+                        'div > div:nth-child(2) > div:nth-child(1) > a > span:nth-child(2) > span').get_text().strip()
+                else:
+                    company_name = company_name_tag.get_text().strip()
+            except Exception as e:
+                company_name = None
+
+            try:
+                company_link = li.select('div > div:nth-child(1) a')[0]['href']
+            except Exception as e:
+                company_link = None
+
+            experience_list.append({"job_title": job_title, "company_name": company_name, "company_link": company_link})
+
+        print("Experience: {}".format(experience_list))
+
+    except Exception as e:
+        print("error getting about: {}".format(e))
+        experience_list = None
+
+    # Get Education of the Person
+    try:
+        education_tag = soup.select_one("#education")
+        education_lis = education_tag.parent.select('div:nth-child(3) > ul > li')
+
+        education_list = []
+        for (i, li) in enumerate(education_lis):
+            try:
+                education_name = li.select_one(
+                    'div > div:nth-child(2) > div:nth-child(1) > a > div span:first-child').get_text()
+            except Exception as e:
+                education_name = None
+
+            try:
+                education_period = li.select_one(
+                    'div > div:nth-child(2) > div:nth-child(1) > a > span:nth-child(3) > span').get_text().strip()
+            except Exception as e:
+                education_period = None
+
+            education_list.append({"name": education_name, "period": education_period})
+
+        print("Education: {}".format(education_list))
+    except Exception as e:
+        print("error getting education: {}".format(e))
+        education_list = None
+
+    # make json
+    # json_data = {
+    #     "name": first_last_name,
+    #     "location": location,
+    #     "about": about,
+    #     "experience": experience_list,
+    #     "education": education_list
+    # }
+
     # info.append([first_last_name, title, company_link, job_title, company_name, talksAbout, location, contacts])
     # time.sleep(5)
 
     print("End of the program")
     browser.quit()
+
+    # process exit
+    exit(0)
 
 
 if __name__ == '__main__':
