24239808980e20decaa83cbbc5755b9afd0c1e5a
kimyk0120 <kimyk0120@hotmail.com>
2024-09-03 17:41:09 +0900

    mod

--- scraper/core/company_person.py
+++ scraper/core/company_person.py
@@ -1,4 +1,5 @@
 import time
+from telnetlib import EC
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -25,7 +26,7 @@ def chrome(headless=False):
     return browser
 
 
-def scraper_from_company(scape_url=None):
+def scraper_from_company(scape_url=None, debug=False):
     # 컴퍼니의 무한 스크롤 페이징이 끝까지 도달하게 한다.
     # 컴퍼니의 사람들의 url을 수집한다.
     # 각 사람들의 url로 이동하여 정보를 수집한다.
@@ -35,8 +36,9 @@ def scraper_from_company(scape_url=None):
 
     test_url = scape_url
     if scape_url is None:
-        # test_url = "https://www.linkedin.com/in/suwaidaslam/"
-        test_url = "https://www.linkedin.com/in/youngkwang-kim-360739244"
+        # https://www.linkedin.com/company/dktechin/people/
+        # https://www.linkedin.com/company/highspot/people/
+        test_url = "https://www.linkedin.com/company/dktechin/people/"
 
     print("Test URL: {}".format(test_url))
 
@@ -50,7 +52,7 @@ def scraper_from_company(scape_url=None):
     browser.get('https://www.linkedin.com/uas/login')
     
     # 아래 예시 코드로 바꿔햐 할듯
-    # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "username")))
+    # element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "username")))
     browser.implicitly_wait(3)
 
     # 로그인 정보가 담긴 파일을 읽어서 로그인
@@ -111,132 +113,15 @@ def scraper_from_company(scape_url=None):
     src = browser.page_source
     soup = BeautifulSoup(src, 'lxml')
 
-    # profile image url
-    try:
-        profile_img = soup.select('section.artdeco-card')[0].select('img')[1]['src']
-        print("profile image url: {}".format(profile_img))
-    except Exception as e:
-        print("error getting profile image url: {}".format(e))
-        profile = None
 
-    # Get Name of the person
-    try:
-        first_last_name = soup.select('section.artdeco-card')[0].select('h1')[0].get_text().strip()
-        print("Name: {}".format(first_last_name))
-    except Exception as e:
-        print("error getting name: {}".format(e))
-        first_last_name = None
-
-    # Get Location of the Person
-    try:
-        location_section = soup.findAll('section', {'class': 'artdeco-card'})[0]
-        location = \
-            location_section.find_all('div', recursive=False)[1].find_all('div', recursive=False)[1].find_all('div',
-                                                                                                              recursive=False)[
-                1].find('span').get_text().strip()
-        print("Location: {}".format(location))
-    except Exception as e:
-        print("error getting location: {}".format(e))
-        location = None
-
-    # !! 소개 섹션부터 section 태그 하위에 div id가 있는데 여기 id 명이 section 명과 같다.
-
-    # Get about of the Person
-    try:
-        about_id_tag = soup.select_one("#about")
-        about = about_id_tag.parent.select_one('div:nth-child(3)').get_text().strip()
-        print("About: {}".format(about))
-    except Exception as e:
-        print("error getting about: {}".format(e))
-        about = None
-
-    # Get Experience of the Person
-    try:
-        experience_tag = soup.select_one("#experience")
-        experience_lis = experience_tag.parent.select('div:nth-child(3) > ul > li')
-
-        experience_list = []
-        for (i, li) in enumerate(experience_lis):
-
-            try:
-                job_title_tag = li.select_one(
-                    'div > div:nth-child(2) > div:nth-child(1) > div > div > div span:first-child')
-                if job_title_tag is None:
-                    job_title = li.select_one(
-                        'div > div:nth-child(2) > div:nth-child(1) > a > div > div > div span:first-child').get_text().strip()
-                else:
-                    job_title = job_title_tag.get_text().strip()
-            except Exception as e:
-                job_title = None
-
-            try:
-                company_name_tag = li.select_one(
-                    'div > div:nth-child(2) > div:nth-child(1) > div > span:nth-child(2) > span')
-                if company_name_tag is None:
-                    company_name = li.select_one(
-                        'div > div:nth-child(2) > div:nth-child(1) > a > span:nth-child(2) > span').get_text().strip()
-                else:
-                    company_name = company_name_tag.get_text().strip()
-            except Exception as e:
-                company_name = None
-
-            try:
-                company_link = li.select('div > div:nth-child(1) a')[0]['href']
-            except Exception as e:
-                company_link = None
-
-            experience_list.append({"job_title": job_title, "company_name": company_name, "company_link": company_link})
-
-        print("Experience: {}".format(experience_list))
-
-    except Exception as e:
-        print("error getting about: {}".format(e))
-        experience_list = None
-
-    # Get Education of the Person
-    try:
-        education_tag = soup.select_one("#education")
-        education_lis = education_tag.parent.select('div:nth-child(3) > ul > li')
-
-        education_list = []
-        for (i, li) in enumerate(education_lis):
-            try:
-                education_name = li.select_one(
-                    'div > div:nth-child(2) > div:nth-child(1) > a > div span:first-child').get_text()
-            except Exception as e:
-                education_name = None
-
-            try:
-                education_period = li.select_one(
-                    'div > div:nth-child(2) > div:nth-child(1) > a > span:nth-child(3) > span').get_text().strip()
-            except Exception as e:
-                education_period = None
-
-            education_list.append({"name": education_name, "period": education_period})
-
-        print("Education: {}".format(education_list))
-    except Exception as e:
-        print("error getting education: {}".format(e))
-        education_list = None
-
-    # make json
-    json_data = {
-        "name": first_last_name,
-        "location": location,
-        "about": about,
-        "experience": experience_list,
-        "education": education_list
-    }
-
-    print("json_data: ", json_data)
 
     print("End of the program")
     browser.quit()
 
-    return json_data
+    return None
 
 
 if __name__ == '__main__':
-    scraper_from_company()
+    scraper_from_company(debug=True)
     exit(0)
 
